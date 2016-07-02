a7b21a6be502ab21b041eb10ef4afad6386489a0
TakayukiABE <s1411344@coins.tsukuba.ac.jp>
2016-07-02 14:45:57 +0900

    modify relations

--- HomeworkManager/Homework.swift
+++ HomeworkManager/Homework.swift
@@ -2,10 +2,10 @@ import UIKit
 import RealmSwift
 class Homework: Object {
     dynamic var id: Int = 0
-    dynamic var subject: Subject = Subject()
+    let subjects: List<Subject> = List<Subject>()
     dynamic var createdAt: NSDate = NSDate()
     dynamic var closeAt: NSDate = NSDate()
     dynamic var finished: Bool = false
-    dynamic var photo: Photo = Photo()
+    let photos: List<Photo> = List<Photo>()
     dynamic var reference: Int = 0
 }
