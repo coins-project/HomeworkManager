56dc7dc035cd17c9c21d3b9326db88291411aa92
Ryo Morimoto <morimoto@m-pc.local>
2016-07-13 13:06:49 +0900

    add InputView

--- HomeworkManager/AppDelegate.swift
+++ HomeworkManager/AppDelegate.swift
@@ -8,6 +8,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        var storyboard = UIStoryboard(name: "Input", bundle: nil)
+        
+        var initialViewController = storyboard.instantiateViewControllerWithIdentifier("InputScreen") as! UIViewController
+        
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
@@ -32,7 +41,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
