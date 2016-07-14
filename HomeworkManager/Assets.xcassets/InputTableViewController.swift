87a049ee63a36a305d2c2c165beede719b8c2067
Ryo Morimoto <morimoto@m-pc.local>
2016-07-14 20:03:35 +0900

    modify InputTableViewController

--- HomeworkManager/Assets.xcassets/InputTableViewController.swift
+++ HomeworkManager/Assets.xcassets/InputTableViewController.swift
@@ -2,8 +2,50 @@ import UIKit
 
 class InputTableViewController: UITableViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
 
-    @IBOutlet weak var SubjectSegmentedControl: UISegmentedControl!
-
+    private let realm = RealmModelManager.sharedManager
+    private var subject = "国語"
+    private var reference = "プリント"
+    @IBAction func subjectSegmentedControl(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            subject = "国語"
+        case 1:
+            subject = "数学"
+        case 2:
+            subject = "理科"
+        case 3:
+            subject = "社会"
+        case 4:
+            subject = "英語"
+        default:
+            subject = ""
+        }
+        
+    }
+    
+    @IBAction func referenceSegmentedControl(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            reference = "プリント"
+        case 1:
+            reference = "教科書"
+        default:
+            reference = ""
+        }
+    }
+    
+    @IBAction func deadlineDatePicker(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "ja_JP")
+        dateFormatter.timeStyle = .NoStyle
+        dateFormatter.dateStyle = .ShortStyle
+        dateFormatter.timeZone = NSTimeZone(abbreviation: "JST")
+        let date = dateFormatter.dateFromString(dateFormatter.stringFromDate(sender.date))
+        print(date)
+    }
+    
+    
+    
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
@@ -18,5 +60,14 @@ class InputTableViewController: UITableViewController,UICollectionViewDelegate,U
         
         return cell
     }
+    
+    @IBAction func saveUIButtonTouchUpInside(sender: UIButton) {
+        let homework = Homework()
+        homework.subjects[0] = subject
+        homework.reference = reference
+        
+        
+    }
+    
     
 }
