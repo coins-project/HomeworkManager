b670af7e6a2d98aa9d1dc06267328ef18afab697
KyoheiKATO <kato@ksfee.org>
2016-11-14 10:59:06 +0900

    Use default realm file in app directory

--- HomeworkManager/AppDelegate.swift
+++ HomeworkManager/AppDelegate.swift
@@ -8,7 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        openDefaultRealm()
         return true
     }
 
@@ -33,20 +32,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-    private func openDefaultRealm() {
-        let defaultPath = Realm.Configuration.defaultConfiguration.fileURL!.path!
-        let bundleRealmPath = NSBundle.mainBundle().pathForResource("default", ofType: "realm")
-        
-        if RealmModelManager.sharedManager.findAllObjects(Subject.self).count == 0 {
-            do {
-                try NSFileManager.defaultManager().removeItemAtPath(defaultPath)
-                try NSFileManager.defaultManager().copyItemAtPath(bundleRealmPath!, toPath: defaultPath)
-            } catch let error as NSError {
-                print("Cannot copy default realm object \(error)")
-            }
-        }
-    }
-
 }
 
