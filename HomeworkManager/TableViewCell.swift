24d73ba005c1f411075d2f4374e40a231f57d3b3
Kazuki Furukawa <khmkazu921@gmail.com>
2016-11-16 16:42:03 +0900

    change checkbox's function

--- HomeworkManager/TableViewCell.swift
+++ HomeworkManager/TableViewCell.swift
@@ -11,38 +11,31 @@ class TableViewCell: UITableViewCell {
     var homework = Homework()
     var realm = RealmModelManager.sharedManager
     
-    //let cell = check.superview as!
-    
-    //http://qiita.com/mzuk/items/0efc41460631c7c98c3c
-    
     func setCell(homework :Homework) {
         self.close.text = DateFormatter.stringFromDate(homework.closeAt)
         self.reference.text = homework.reference
         self.subject.text = homework.subject?.name
         self.check.addTarget(self, action: #selector(TableViewCell.tapCheckButton(_:)), forControlEvents: .TouchUpInside)
+        self.backgroundColor = UIColor.hexStr(homework.subject!.hexColor, alpha: 1)
+        self.homework = homework
+        changeCheckButton(homework)
+    }
+    
+    func tapCheckButton(sender: UIButton){
+        check.selected = !check.selected
+        realm.update(homework, value: ["finished": check.selected])
+        changeCheckButton(homework)
+        print(homework.subject?.name)
+        print(homework.finished)
+    }
+    
+    func changeCheckButton(homework: Homework) {
         if(homework.finished) {
             self.check.setImage(UIImage(named: "checked.png"), forState: UIControlState.Selected)
         } else {
             self.check.setImage(UIImage(named: "unchecked.png"), forState: UIControlState.Normal)
         }
-        self.backgroundColor = UIColor.hexStr(homework.subject!.hexColor, alpha: 1)
-        self.homework = homework
     }
-    
-    internal func tapCheckButton(sender: UIButton){
-        check.selected = !check.selected
-        
-        homework.finished = !homework.finished
-        //ここでトランザクションが走る
-        realm.update(homework)
-        
-        
-//        self.superview!.superview.reloadData()
-        
-        print(homework.subject?.name)
-        print(homework.finished)
-    }
-    
     
     
 }
