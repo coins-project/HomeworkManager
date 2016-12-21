b9cd13c31b412a978d0a79669625c96a0f75cdf1
Kazuki Furukawa <khmkazu921@gmail.com>
2016-12-21 15:13:35 +0900

    start creating color panel

---
+++ HomeworkManager/ColorPanelCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ColorPanelCollectionViewCell.swift
+//  HomeworkManager
+//
+//  Created by 古川 和輝 on 2016/12/21.
+//  Copyright © 2016年 takayuki abe. All rights reserved.
+//
+
+import UIKit
+
+class ColorPanelCollectionViewCell: UICollectionViewCell {
+    
+}
