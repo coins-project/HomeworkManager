61454df6f3fce050f488616971487137e6fe39e2
Kazuki Furukawa <khmkazu921@gmail.com>
2017-01-07 20:56:39 +0900

    it can display subject's list

--- HomeworkManager/EditSubjectViewController.swift
+++ HomeworkManager/EditSubjectViewController.swift
@@ -1,50 +1,35 @@
 import UIKit
-//import RealmSwift
+import RealmSwift
 
 class EditSubjectViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    @IBOutlet weak var tableView: UITableView!
     
-//    var subject = Subject()
-//    private let realm = RealmModelManager.sharedManager
+    private var subjects :Results<Subject>?
+    private let realm = RealmModelManager.sharedManager
+    private var update = false
+    var subjectName :String
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        do{
+            let realm = try Realm()
+            subjects = realm.objects(Subject)
+            tableView.reloadData()
+        }catch{
+            print("error")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return subjects!.count
     }
     
-    /// セルに値を設定するデータソースメソッド（必須）
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        return UITableViewCell()
-    }
-    
-    /// セルが選択された時に呼ばれるデリゲートメソッド
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {}
-
-    
-    /*
-    func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var subject:Subject? = nil
         do{
             let realm = try Realm()
@@ -53,23 +38,27 @@ class EditSubjectViewController: UIViewController, UITableViewDelegate, UITableV
             print("エラー")
         }
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("", forIndexPath: indexPath) as! EditSubjectViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("Subject", forIndexPath: indexPath) as! EditSubjectViewCell
         cell.subject.text = subject!.name
         cell.backgroundColor = UIColor.hexStr(subject!.hexColor, alpha: 1)
-
+        
         return cell
     }
-
-*/
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 0
-//    }
-
+    
+    func tableView(table: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        subjectName = subjects![indexPath.row].name
+        performSegueWithIdentifier("editSubject",sender: nil)
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-        if (segue.identifier == "editDetail") {
+        if (segue.identifier == "editSubject") {
             let colorPanel: ColorPanelViewController = (segue.destinationViewController as? ColorPanelViewController)!
-            let text = "あいうえお"
-            colorPanel.subjectName.text = text
+            colorPanel.subjectName.text = subjectName
+            //colorPanel.
         }
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 }
