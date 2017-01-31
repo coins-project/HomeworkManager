6e0ee28a09cb0227c693ca2afb49d89306768e2c
Kazuki Furukawa <khmkazu921@gmail.com>
2017-01-31 17:10:49 +0900

    Revert "Furukawa/add new subject input"

--- HomeworkManager/HexColorExtention.swift
+++ HomeworkManager/HexColorExtention.swift
@@ -1,6 +1,4 @@
 import UIKit
-import Foundation
-
 extension UIColor {
     class func hexStr ( hexStr : String, alpha : CGFloat) -> UIColor {
         let scanner = NSScanner(string: hexStr)
@@ -12,17 +10,7 @@ extension UIColor {
             return UIColor(red:r,green:g,blue:b,alpha:alpha)
         } else {
             print("invalid hex string")
-            return UIColor.whiteColor()
+            return UIColor.whiteColor();
         }
-    }
-    
-    func strHex() -> String {
-        var r:CGFloat = 0
-        var g:CGFloat = 0
-        var b:CGFloat = 0
-        var a:CGFloat = 0
-        self.getRed(&r, green: &g, blue: &b, alpha: &a)
-        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
-        return String(format:"%06x", rgb)
     }
 }
